0f2b0f59a7844cdaf699aaab81f5c5f257a2a2fd
ryuseiy <c0b2215016@edu.teu.ac.jp>
2023-06-20 14:38:59 +0900

    練習3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,6 +4,12 @@ import pygame as pg
 
 
 WIDTH, HEIGHT = 1600, 900
+delta = {
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0)
+}
 
 
 def main():
@@ -12,6 +18,8 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rect = kk_img.get_rect()
+    kk_rect.center = 900, 400
     r_bomb = pg.Surface((20, 20))
     pg.draw.circle(r_bomb, (255, 0, 0), (10, 10), 10)
     r_bomb.set_colorkey((0, 0, 0))
@@ -21,20 +29,26 @@ def main():
     rb_rect.center = rx, ry
     vx, vy = +5, +5
     clock = pg.time.Clock()
-    clock.tick(50)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-
+        
+        key_list = pg.key.get_pressed()
+        sum_mv = [0, 0]
+        for k, mv in delta.items():
+            if key_list[k]:
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+            kk_rect.move_ip(sum_mv)
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+        screen.blit(kk_img, kk_rect)
         rb_rect.move_ip(vx, vy)
         screen.blit(r_bomb, rb_rect)
         pg.display.update()
         tmr += 1
-        clock.tick(10)
+        clock.tick(50)
 
 
 if __name__ == "__main__":
